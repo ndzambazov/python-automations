710334e0f0a632ccb2a97f44531e27fe150acf6c
Nikolay Dzambazov <88478165+nikdzambazov@users.noreply.github.com>
2024-04-15 14:00:37 +0300

    youtube downloader update

--- youtube-downloader/downloader.py
+++ youtube-downloader/downloader.py
@@ -1,12 +1,15 @@
 from pytube import YouTube
 from sys import argv
+import os
 
+download_dir = os.path.join(os.path.expanduser("~"), "Downloads")
 link = argv[1]
 
 yt = YouTube(link)
 
 print("Title: ", yt.title)
 print("Views: ", yt.views)
+print("Download folder: ", download_dir)
 
 yd = yt.streams.get_highest_resolution()
-yd.download('Downloads/')
+yd.download(download_dir)
